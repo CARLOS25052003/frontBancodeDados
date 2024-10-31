5d2ef9ec71e990e01cd5cf30167249449b37e03d
Carlos Charles <carloscharles016@outlook.com>
2024-10-31 13:44:48 -0400

    Incrementação de datas, agora falta incrementar ,

--- Index/app.js
+++ Index/app.js
@@ -1,26 +1,42 @@
+// Seletores do DOM
 const rendaExtraForm = document.getElementById('rendaExtraForm');
+
 const despesaForm = document.getElementById('despesaForm');
+
 const rendaFixaForm = document.getElementById('rendaFixaForm');
+
+const despesaFixaForm = document.getElementById('despesaFixaForm');
+
 const rendaExtraList = document.getElementById('rendaExtraList');
+
 const despesaList = document.getElementById('despesaList');
+
 const rendaFixaList = document.getElementById('rendaFixaList');
+
+const despesaFixaList = document.getElementById('despesaFixaList');
+
 const balanceDisplay = document.getElementById('balance');
 
 let totalBalance = 0;
 
 // Função para formatar a data para o formato desejado (dia/mês/ano)
 function formatDateToDMY(date) {
-    const [year, month, day] = date.split('-');
+    const [day, month, year] = date.split('/');
     return `${day}/${month}/${year}`;
 }
 
-// Adicionar Renda Extra
+// Atualiza o saldo na tela
+function updateBalance() {
+    balanceDisplay.textContent = `Saldo Total: R$ ${totalBalance.toFixed(2)}`;
+}
+
+// Adiciona uma renda extra
 rendaExtraForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     const rendaExtraDescription = document.getElementById('rendaExtraDescription').value.trim();
     const rendaExtraAmount = document.getElementById('rendaExtraAmount').value.replace(",", ".");
-    const rendaExtraDate = formatDateToDMY(document.getElementById('rendaExtraDate').value); // Formata a data
+    const rendaExtraDate = document.getElementById('rendaExtraDate').value;
 
     try {
         const response = await fetch('http://localhost:8080/renda-extra', {
@@ -29,7 +45,7 @@ rendaExtraForm.addEventListener('submit', async (e) => {
             body: JSON.stringify({
                 description: rendaExtraDescription,
                 amount: parseFloat(rendaExtraAmount),
-                date_added: rendaExtraDate // Usa a data já no formato correto
+                dateAdded: rendaExtraDate
             }),
         });
 
@@ -47,12 +63,12 @@ rendaExtraForm.addEventListener('submit', async (e) => {
     }
 });
 
-// Adicionar Renda Fixa
+// Adiciona uma renda fixa
 rendaFixaForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const rendaFixaDescription = document.getElementById('rendaFixaDescription').value;
     const rendaFixaAmount = document.getElementById('rendaFixaAmount').value.replace(",", ".");
-    const rendaFixaDate = formatDateToDMY(document.getElementById('rendaFixaDate').value); // Formata a data
+    const rendaFixaDate = document.getElementById('rendaFixaDate').value;
 
     try {
         const response = await fetch('http://localhost:8080/renda_fixa', {
@@ -61,7 +77,7 @@ rendaFixaForm.addEventListener('submit', async (e) => {
             body: JSON.stringify({
                 description: rendaFixaDescription,
                 amount: parseFloat(rendaFixaAmount),
-                date_added: rendaFixaDate // Usa a data já no formato correto
+                dateAdded: rendaFixaDate
             }),
         });
 
@@ -79,12 +95,12 @@ rendaFixaForm.addEventListener('submit', async (e) => {
     }
 });
 
-// Adicionar Despesa
+// Adiciona uma despesa
 despesaForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const despesaDescription = document.getElementById('despesaDescription').value;
     const despesaAmount = document.getElementById('despesaAmount').value.replace(",", ".");
-    const despesaDate = formatDateToDMY(document.getElementById('despesaDate').value); // Formata a data
+    const despesaDate = document.getElementById('despesaDate').value;
 
     try {
         const response = await fetch('http://localhost:8080/despesas', {
@@ -93,7 +109,7 @@ despesaForm.addEventListener('submit', async (e) => {
             body: JSON.stringify({
                 description: despesaDescription,
                 amount: parseFloat(despesaAmount),
-                date_added: despesaDate // Usa a data já no formato correto
+                dateAdded: despesaDate
             }),
         });
 
@@ -111,12 +127,15 @@ despesaForm.addEventListener('submit', async (e) => {
     }
 });
 
-// Adicionar Despesa Fixa
+// Adiciona uma despesa fixa
 despesaFixaForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const despesaFixaDescription = document.getElementById('despesaFixaDescription').value;
     const despesaFixaAmount = document.getElementById('despesaFixaAmount').value.replace(",", ".");
-    const despesaFixaDate = formatDateToDMY(document.getElementById('despesaFixaDate').value); // Formata a data
+    const despesaFixaDate = formatDateToDMY(document.getElementById('despesaFixaDate').value);
+
+    const [day, month, year] = despesaFixaDate.split('/');
+    const formattedDate = `${year}-${month}-${day}`; // Formato AAAA-MM-DD
 
     try {
         const response = await fetch('http://localhost:8080/despesas-fixas', {
@@ -125,7 +144,7 @@ despesaFixaForm.addEventListener('submit', async (e) => {
             body: JSON.stringify({
                 description: despesaFixaDescription,
                 amount: parseFloat(despesaFixaAmount),
-                date_added: despesaFixaDate // Usa a data já no formato correto
+                dateAdded: despesaFixaDate
             }),
         });
 
@@ -143,18 +162,12 @@ despesaFixaForm.addEventListener('submit', async (e) => {
     }
 });
 
-// Função para atualizar o saldo na tela
-function updateBalance() {
-    balanceDisplay.textContent = `Saldo Total: R$ ${totalBalance.toFixed(2)}`;
-}
-
 // Função para exibir a renda extra na lista
 function displayRendaExtra(rendaExtra) {
     const li = document.createElement('li');
     li.style.display = 'flex';
     li.style.justifyContent = 'space-between';
-
-    li.textContent = `Renda Extra: R$ ${rendaExtra.amount.toFixed(2)}`;
+    li.textContent = `${rendaExtra.description}: R$ ${rendaExtra.amount.toFixed(2)} - ${rendaExtra.dateAdded}`;
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Excluir';
@@ -170,8 +183,7 @@ function displayDespesa(despesa) {
     const li = document.createElement('li');
     li.style.display = 'flex';
     li.style.justifyContent = 'space-between';
-
-    li.textContent = `${despesa.description}: R$ ${despesa.amount.toFixed(2)}`;
+    li.textContent = `${despesa.description}: R$ ${despesa.amount.toFixed(2)} - ${despesa.dateAdded}`;
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Excluir';
@@ -187,8 +199,7 @@ function displayRendaFixa(rendaFixa) {
     const li = document.createElement('li');
     li.style.display = 'flex';
     li.style.justifyContent = 'space-between';
-
-    li.textContent = `${rendaFixa.description}: R$ ${rendaFixa.amount.toFixed(2)}`;
+    li.textContent = `${rendaFixa.description}: R$ ${rendaFixa.amount.toFixed(2)} - ${rendaFixa.dateAdded}`;
 
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Excluir';
@@ -199,12 +210,28 @@ function displayRendaFixa(rendaFixa) {
     rendaFixaList.appendChild(li);
 }
 
-// Funções para deletar renda extra, despesa, renda fixa
+// Função para exibir a despesa fixa na lista
+function displayDespesaFixa(despesaFixa) {
+    const li = document.createElement('li');
+    li.style.display = 'flex';
+    li.style.justifyContent = 'space-between';
+    li.textContent = `${despesaFixa.description}: R$ ${despesaFixa.amount.toFixed(2)} - ${despesaFixa.dateAdded}`;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Excluir';
+    deleteButton.className = 'delete-button';
+    deleteButton.addEventListener('click', () => deleteDespesaFixa(despesaFixa.id, despesaFixa.amount));
+    li.appendChild(deleteButton);
+
+    despesaFixaList.appendChild(li);
+}
+
+// Funções para deletar renda extra, despesa, renda fixa e despesa fixa
 async function deleteRendaExtra(id, amount) {
     try {
         const response = await fetch(`http://localhost:8080/renda-extra/${id}`, { method: 'DELETE' });
         if (response.ok) {
-            totalBalance -= amount;
+            totalBalance -= amount; // Subtrai a renda extra ao excluir
             updateBalance();
             loadData();
         } else {
@@ -219,7 +246,7 @@ async function deleteDespesa(id, amount) {
     try {
         const response = await fetch(`http://localhost:8080/despesas/${id}`, { method: 'DELETE' });
         if (response.ok) {
-            totalBalance += amount;
+            totalBalance += amount; // Adiciona a despesa ao excluir
             updateBalance();
             loadData();
         } else {
@@ -234,7 +261,7 @@ async function deleteRendaFixa(id, amount) {
     try {
         const response = await fetch(`http://localhost:8080/renda_fixa/${id}`, { method: 'DELETE' });
         if (response.ok) {
-            totalBalance -= amount;
+            totalBalance -= amount; // Subtrai a renda fixa ao excluir
             updateBalance();
             loadData();
         } else {
@@ -245,33 +272,100 @@ async function deleteRendaFixa(id, amount) {
     }
 }
 
+async function deleteDespesaFixa(id, amount) {
+    try {
+        const response = await fetch(`http://localhost:8080/despesas-fixas/${id}`, { method: 'DELETE' });
+        if (response.ok) {
+            totalBalance += amount; // Adiciona a despesa fixa ao excluir
+            updateBalance();
+            loadData();
+        } else {
+            console.error('Erro ao deletar despesa fixa:', await response.text());
+        }
+    } catch (error) {
+        console.error('Erro ao deletar despesa fixa:', error);
+    }
+}
+
 // Função para carregar os dados
 async function loadData() {
     // Limpa as listas
     rendaExtraList.innerHTML = '';
     despesaList.innerHTML = '';
     rendaFixaList.innerHTML = '';
+    despesaFixaList.innerHTML = '';
 
-    // Carregar Renda Extra
+    // Carregar e exibir Renda Extra
     const responseRendaExtra = await fetch('http://localhost:8080/renda-extra');
     if (responseRendaExtra.ok) {
         const rendaExtraItems = await responseRendaExtra.json();
         rendaExtraItems.forEach(displayRendaExtra);
     }
 
-    // Carregar Despesas
+    // Carregar e exibir Despesas
     const responseDespesas = await fetch('http://localhost:8080/despesas');
     if (responseDespesas.ok) {
         const despesaItems = await responseDespesas.json();
         despesaItems.forEach(displayDespesa);
     }
 
-    // Carregar Renda Fixa
+    // Carregar e exibir Renda Fixa
     const responseRendaFixa = await fetch('http://localhost:8080/renda_fixa');
     if (responseRendaFixa.ok) {
         const rendaFixaItems = await responseRendaFixa.json();
         rendaFixaItems.forEach(displayRendaFixa);
     }
+
+    // Carregar e exibir Despesas Fixas
+    const responseDespesaFixa = await fetch('http://localhost:8080/despesas-fixas');
+    if (responseDespesaFixa.ok) {
+        const despesaFixaItems = await responseDespesaFixa.json();
+        despesaFixaItems.forEach(displayDespesaFixa);
+    }
+
+    // Calcular o saldo total
+    await calculateTotalBalance();
+}
+
+// Função para calcular o saldo total
+async function calculateTotalBalance() {
+    const responseRendaExtra = await fetch('http://localhost:8080/renda-extra');
+    const responseDespesas = await fetch('http://localhost:8080/despesas');
+    const responseRendaFixa = await fetch('http://localhost:8080/renda_fixa');
+    const responseDespesaFixa = await fetch('http://localhost:8080/despesas-fixas');
+
+    let rendaExtraTotal = 0;
+    let despesasTotal = 0;
+    let rendaFixaTotal = 0;
+    let despesaFixaTotal = 0;
+
+    // Calcular a soma das rendas extras
+    if (responseRendaExtra.ok) {
+        const rendaExtraItems = await responseRendaExtra.json();
+        rendaExtraTotal = rendaExtraItems.reduce((sum, item) => sum + item.amount, 0);
+    }
+
+    // Calcular a soma das despesas
+    if (responseDespesas.ok) {
+        const despesaItems = await responseDespesas.json();
+        despesasTotal = despesaItems.reduce((sum, item) => sum + item.amount, 0);
+    }
+
+    // Calcular a soma das rendas fixas
+    if (responseRendaFixa.ok) {
+        const rendaFixaItems = await responseRendaFixa.json();
+        rendaFixaTotal = rendaFixaItems.reduce((sum, item) => sum + item.amount, 0);
+    }
+
+    // Calcular a soma das despesas fixas
+    if (responseDespesaFixa.ok) {
+        const despesaFixaItems = await responseDespesaFixa.json();
+        despesaFixaTotal = despesaFixaItems.reduce((sum, item) => sum + item.amount, 0);
+    }
+
+    // Calcular o saldo total
+    totalBalance = rendaExtraTotal + rendaFixaTotal - (despesasTotal + despesaFixaTotal);
+    updateBalance(); // Atualiza o saldo na tela
 }
 
 // Carrega os dados ao iniciar
